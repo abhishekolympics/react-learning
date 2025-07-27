af52bb18d2b548724ba14140e1b0d34a4523e624
Your-Personal-Name <abhishekolympics@gmail.com>
2025-07-27 19:16:52 +0530

    feat: enhance task management with edit functionality

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
 import { useState } from "react";
 
 function App() {
-
   const [tasks, setTasks] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
+  const [update, setUpdate] = useState(false);
+  const [updateIndex, setUpdateIndex] = useState(-1);
+  const [updateValue, setUpdateValue] = useState("");
 
-  const handleAddTask = async() => {
-    if(input.trim() === '') return alert("empty task details");
+  const handleAddTask = async () => {
+    if (input.trim() === "") return alert("empty task details");
     setTasks([...tasks, input]);
-    setInput('');
-  }
+    setInput("");
+  };
 
   const handleDeleteTask = async (index) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
-  }
+  };
 
-  // const handleEditTask = async () => {
-  //   tasks[s].push(newString);
-  // }
+  const handleEditTask = (task, index) => {
+    setUpdate(true);
+    setUpdateIndex(index);
+    setUpdateValue(task);
+  };
+  const handleEditTask1 = async (index, updatedText) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = updatedText;
+    setTasks(updatedTasks);
+    setUpdate(false);
+    setUpdateIndex(-1)
+    setUpdateValue("");
+  };
 
-  return ( <>
-     <h2> Task Manager</h2>
-     <input 
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-      placeholder="Enter task title"
-      />
-      <button onClick={handleAddTask}>Add task</button>
+  return (
+    <>
+      <h2> Task Manager</h2>
+
+      {update ? (
+        <>
+          <input
+            value={updateValue}
+            onChange={(e) => setUpdateValue(e.target.value)}
+            placeholder={tasks[updateIndex]}
+          />
+          <button onClick={() => handleEditTask1(updateIndex, updateValue)}>Update task</button>
+        </>
+      ) : (
+        <>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Enter task title"
+          />
+          <button onClick={handleAddTask}>Add task</button>
+        </>
+      )}
 
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
             {tasks[index]}
-            <button onClick={() => handleDeleteTask(index)}> Delete task </button>
+            <button onClick={() => handleDeleteTask(index)}>
+              {" "}
+              Delete task{" "}
+            </button>
+            <button
+              onClick={() => {
+                handleEditTask(task, index);
+              }}
+            >
+              Edit
+            </button>
           </li>
         ))}
       </ul>
