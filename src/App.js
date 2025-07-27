a837f506d96ae7cea70a940cca5a7cae8bf36e0f
Your-Personal-Name <abhishekolympics@gmail.com>
2025-07-27 18:15:25 +0530

    feat: implement task management functionality in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function App() {
-  return <p> Simple React app.</p>;
+
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  const handleAddTask = async() => {
+    if(input.trim() === '') return alert("empty task details");
+    setTasks([...tasks, input]);
+    setInput('');
+  }
+
+  const handleDeleteTask = async (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  // const handleEditTask = async () => {
+  //   tasks[s].push(newString);
+  // }
+
+  return ( <>
+     <h2> Task Manager</h2>
+     <input 
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      placeholder="Enter task title"
+      />
+      <button onClick={handleAddTask}>Add task</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {tasks[index]}
+            <button onClick={() => handleDeleteTask(index)}> Delete task </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
